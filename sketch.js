f4dcd387006aa53ed4d149d2394c9100f1bdf1ce
PINKPHANTOMS <55456019+PINKPHANTOMS@users.noreply.github.com>
2020-05-05 22:46:42 -0700

    klvds

--- sketch.js
+++ sketch.js
@@ -1,82 +1,65 @@
-let gif;
-let graphics;
-let vS;
-// let gif1
-// let gif2
+let body;
+let zoom;
+let playing = false;
 
 function preload(){
 
-	// song = loadSound('thisdot.mp3');
+	body = createVideo('DMDocBody.mp4'); //loads video
+	body.volume(0); //sets volume to zero to comply with CORS
 
-	vS = loadModel('smallboi.stl'); //loads model - insert file path into quotations
+	zoom = createVideo('dMDocZoom.mp4');
+	zoom.volume(0);
 
-	gif = createVideo('no.mov'); //loads video
-	gif.volume(0); //sets volume to zero to comply with CORS
+	body.hide(); //hides mov file
+	zoom.hide();
+
 
 }
 
 
 function setup(){
 
-	createCanvas(windowWidth, windowHeight, WEBGL);
+	canvas = createCanvas(windowWidth, windowHeight);
 
-	graphics = createGraphics(1000, 1000); //loads separate canvas off screen - named 'graphics'
-	gif.hide(); //hides mov file
-	gif.loop(); //loops the mov file
-
-	// fft = new p5.FFT();
 }
 
 function draw(){
 
-	
-
-
-	// graphics.fill(0,0,0,150);
-	// graphics.tint(255, 100); // Apply transparency without changing color
-
-	graphics.image(gif, 0, 0, 1000, 1000); //draws movie on the entirety of the graphics canvas
-
 	background(0,0,0,0);
 
-	ambientLight(56, 56, 56); //this code block adds some ambient light
-	ambientMaterial(random(200), random(100));
-	let dirX = (mouseX / width - 0.5) * 2;
-  	let dirY = (mouseY / height - 0.5) * 2;
-  	directionalLight(250, 250, 250, -dirX, -dirY, -1);
+	scale(0.3);
 
-  	camera(0, 0, -1500, 0, 0, 0, 0, 1, 0); //sets object back in z-direction
+	let s = "beginShape();{The Matrix; The Social Network; Tron; Ideologues of silicon valley. My own inspirations, all told; ideology I insufferably bathe in - but I suppose I'm not alone, as we all stop in our critique of ideology at some point. It's then I'm reminded of <i>They Live</i> - we like ideology - it distorts reality to give us something that pleases - we are soaking wet in our dreams, even the pretentious postmodern a-truthers. Who wrote your smut?.....} endShape();";
+	fill(255);
+	textStyle(ITALIC);
+	strokeWeight(1);
+	textSize(60);
+	text(s, width/2, (height/2)+1400, width*2); // Text wraps within text box
 
-  	rotateX(millis()/1000); //rotation code block
-  	rotateY(millis()/1000);
-  	rotateZ(millis()/1000);
-  	scale(50);
+	let t = "click to begin;"
+	text(t, (width/2)+800, height/2+800, 600)
 
-  	texture(graphics); //textures following 3D object with graphics
-	model(vS); //draws model vS
-
-  	// let waveform = fft.waveform();
-
-	// beginShape();
-	//   for (var i = 0; i< waveform.length; i++){
-	//     let x = map(i, 0, waveform.length, 0, width);
-	//     let y = map( waveform[i], -1, 1, 0, height);
-	//     stroke(0);
-	//     texture(graphics);
-	// 	box(y/2)
-	// }
-	// endShape();
-
+	if(playing == true){
+	image(body, width/2, 200);
+	image(zoom, width/2, 1300);
+}
 
 }
 
-// async function mousePressed(){
-// 	if (song.isPlaying()) {
-//     song.stop();
-//   } else {
-//     song.play();
-//   }
-// }
+function mousePressed(){
+	  clear();
+
+	if(playing == false){
+		playing = true;
+		body.play();
+		zoom.play();
+	}else{
+		playing = false;
+		body.stop();
+		zoom.stop();
+	}
+	print(playing);
+}
 
 // function touchStarted(){
 // 	mousePressed()
